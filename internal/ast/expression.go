f653ac79d0ec7e14847c05e7286718368346ebfc
umed-hotamov <umed.hotamov5858@gmail.com>
2025-05-08 13:49:06 +0500

    expressions added

---
+++ internal/ast/expression.go
@@ -0,0 +1,49 @@
+package ast
+
+import (
+	"fmt"
+
+	"github.com/umed-hotamov/golox/internal/lexer"
+)
+
+type Expr interface {
+  String() string
+}
+  
+type Binary struct {
+  Left     Expr
+  Operator lexer.Token
+  Right    Expr
+}
+
+type Grouping struct {
+  Expr Expr
+}
+
+type Literal struct {
+  Value any
+}
+
+type Unary struct {
+  Operator lexer.Token
+  Right    Expr
+}
+
+func (b *Binary) String() string {
+  return fmt.Sprintf("(%v %v %v)", b.Operator.Lexeme, b.Left.String(), b.Right.String())
+}
+
+func (g *Grouping) String() string {
+  return fmt.Sprintf("(group %v)", g.Expr.String())
+}
+
+func (l *Literal) String() string {
+  if l.Value == nil {
+    return "nil"
+  }
+  return fmt.Sprintf("%v", l.Value)
+}
+
+func (u *Unary) String() string {
+  return fmt.Sprintf("(%v %v)", u.Operator.Lexeme, u.Right.String())
+}
