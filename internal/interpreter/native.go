1d20721134c86541d4c2d7457db45544aa06160e
umed-hotamov <umed.hotamov5858@gmail.com>
2025-05-28 19:40:35 +0500

    native init: clock() added

---
+++ internal/interpreter/native.go
@@ -0,0 +1,14 @@
+package interpreter
+
+import "time"
+
+type Clock struct {
+}
+
+func (c Clock) arity() int {
+  return 0
+}
+
+func (c Clock) call(interpreter *Interpreter, arguments []any) any {
+  return float64(time.Now().Second())
+}
