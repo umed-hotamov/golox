e40230d300e66a1491ff18becdb988c2207e0ea5
umed-hotamov <umed.hotamov5858@gmail.com>
2025-06-02 19:33:16 +0500

    add control flow statements, function declaration statement and return statement, add call expression

--- internal/parser/expression.go
+++ internal/parser/expression.go
@@ -7,87 +7,87 @@ import (
 )
 
 type Expr interface {
-  String() string
+	String() string
 }
-  
+
 type Binary struct {
-  Left     Expr
-  Operator lexer.Token
-  Right    Expr
+	Left     Expr
+	Operator lexer.Token
+	Right    Expr
 }
 
 type Grouping struct {
-  Expr Expr
+	Expr Expr
 }
 
 type Literal struct {
-  Value any
+	Value any
 }
 
 type Unary struct {
-  Operator lexer.Token
-  Right    Expr
+	Operator lexer.Token
+	Right    Expr
 }
 
 type Variable struct {
-  Name lexer.Token
+	Name lexer.Token
 }
 
 type Assign struct {
-  Name  lexer.Token
-  Value Expr
+	Name  lexer.Token
+	Value Expr
 }
 
 type Logical struct {
-  Left     Expr
-  Operator lexer.Token
-  Right    Expr
+	Left     Expr
+	Operator lexer.Token
+	Right    Expr
 }
 
 type Call struct {
-  Callee    Expr
-  Paren     lexer.Token
-  Arguments []Expr
+	Callee    Expr
+	Paren     lexer.Token
+	Arguments []Expr
 }
 
 func (b Binary) String() string {
-  return fmt.Sprintf("(%v %v %v)", b.Operator.Lexeme, b.Left.String(), b.Right.String())
+	return fmt.Sprintf("(%v %v %v)", b.Operator.Lexeme, b.Left.String(), b.Right.String())
 }
 
 func (g Grouping) String() string {
-  return fmt.Sprintf("(group %v)", g.Expr.String())
+	return fmt.Sprintf("(group %v)", g.Expr.String())
 }
 
 func (l Literal) String() string {
-  if l.Value == nil {
-    return "nil"
-  }
-  return fmt.Sprintf("%v", l.Value)
+	if l.Value == nil {
+		return "nil"
+	}
+	return fmt.Sprintf("%v", l.Value)
 }
 
 func (u Unary) String() string {
-  return fmt.Sprintf("(%v %v)", u.Operator.Lexeme, u.Right.String())
+	return fmt.Sprintf("(%v %v)", u.Operator.Lexeme, u.Right.String())
 }
 
 func (v Variable) String() string {
-  return fmt.Sprint(v.Name.Lexeme)
+	return fmt.Sprint(v.Name.Lexeme)
 }
 
 func (a Assign) String() string {
-  return fmt.Sprintf("(%v %v)", a.Value.String(), a.Name)
+	return fmt.Sprintf("(%v %v)", a.Value.String(), a.Name)
 }
 
 func (l Logical) String() string {
-  return fmt.Sprintf("%v %v %v", l.Left.String(), l.Operator.Lexeme, l.Right.String())
+	return fmt.Sprintf("%v %v %v", l.Left.String(), l.Operator.Lexeme, l.Right.String())
 }
 
 func (c Call) String() string {
-  s := fmt.Sprintf("%v", c.Callee.String())
-  s += "("
-  for _, a := range c.Arguments {
-    s += a.String()
-  }
-  s += ")"
+	s := fmt.Sprintf("%v", c.Callee.String())
+	s += "("
+	for _, a := range c.Arguments {
+		s += a.String()
+	}
+	s += ")"
 
-  return s
+	return s
 }
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -8,299 +8,397 @@ import (
 )
 
 type Parser struct {
-  tokens     []*lexer.Token
-  statements []Stmt
+	tokens     []*lexer.Token
+	statements []Stmt
 
-  current    int
-  HasError   bool
+	current  int
+	HasError bool
 }
 
 func NewParser(tokens []*lexer.Token) *Parser {
-  return &Parser{
-    tokens: tokens,
-  }
+	return &Parser{
+		tokens: tokens,
+	}
 }
 
 func (p *Parser) Parse() []Stmt {
-  for !p.eof() {
-    p.statements = append(p.statements, p.declaration())
-  }
-  
-  return p.statements
+	for !p.eof() {
+		p.statements = append(p.statements, p.declaration())
+	}
+
+	return p.statements
 }
 
 func (p *Parser) declaration() Stmt {
-  defer p.errorRecovery()
-  
-  if p.match(lexer.VAR) {
-    return p.varDeclaration()
-  }
+	defer p.errorRecovery()
 
-  return p.statement()
+	if p.match(lexer.VAR) {
+		return p.varDeclaration()
+	}
+
+	return p.statement()
 }
 
 func (p *Parser) varDeclaration() Stmt {
-  name := p.acceptToken(lexer.IDENTIFIER, "Expect variable name")
-  
-  var initializer Expr
-  if p.match(lexer.EQUAL) {
-    initializer = p.expression()
-  }
-  p.acceptToken(lexer.SEMICOLON, "Expect ; after variable declaration")
+	name := p.acceptToken(lexer.IDENTIFIER, "Expect variable name")
 
-  return Var{*name, initializer}
+	var initializer Expr
+	if p.match(lexer.EQUAL) {
+		initializer = p.expression()
+	}
+	p.acceptToken(lexer.SEMICOLON, "Expect ; after variable declaration")
+
+	return Var{*name, initializer}
 }
 
 func (p *Parser) statement() Stmt {
-  if p.match(lexer.PRINT)      { return p.printStatement() }
-  if p.match(lexer.LEFT_BRACE) { return p.block()          }
-  if p.match(lexer.IF)         { return p.ifStatement()    }
-  if p.match(lexer.WHILE)      { return p.whileStatement() }
-  if p.match(lexer.FOR)        { return p.forStatement()   }
+	if p.match(lexer.PRINT) {
+		return p.printStatement()
+	}
+	if p.match(lexer.LEFT_BRACE) {
+		return p.block()
+	}
+	if p.match(lexer.IF) {
+		return p.ifStatement()
+	}
+	if p.match(lexer.WHILE) {
+		return p.whileStatement()
+	}
+	if p.match(lexer.FOR) {
+		return p.forStatement()
+	}
+	if p.match(lexer.RETURN) {
+		return p.returnStatement()
+	}
+	if p.match(lexer.FUN) {
+		return p.function("function")
+	}
 
-  return p.expressionStatement()
+	return p.expressionStatement()
 }
 
 func (p *Parser) printStatement() Stmt {
-  value := p.expression()
-  p.acceptToken(lexer.SEMICOLON, "Expect ; after value")
+	value := p.expression()
+	p.acceptToken(lexer.SEMICOLON, "Expect ; after value")
 
-  return Print{value}
+	return Print{value}
 }
 
 func (p *Parser) block() Block {
-  var statements []Stmt
+	var statements []Stmt
 
-  for !p.eof() && !p.check(lexer.RIGHT_BRACE) {
-    statements = append(statements, p.declaration())
-  }
+	for !p.eof() && !p.check(lexer.RIGHT_BRACE) {
+		statements = append(statements, p.declaration())
+	}
 
-  p.acceptToken(lexer.RIGHT_BRACE, "Expect '}' after block")
+	p.acceptToken(lexer.RIGHT_BRACE, "Expect '}' after block")
 
-  return Block{statements} 
+	return Block{statements}
 }
 
 func (p *Parser) expressionStatement() Stmt {
-  expr := p.expression()
-  p.acceptToken(lexer.SEMICOLON, "Expect ; after expression")
+	expr := p.expression()
+	p.acceptToken(lexer.SEMICOLON, "Expect ; after expression")
 
-  return Expression{expr}
+	return Expression{expr}
 }
 
 func (p *Parser) ifStatement() Stmt {
-  p.acceptToken(lexer.LEFT_PAREN, "Expect ( after 'if')")
-  condition := p.expression()
-  p.acceptToken(lexer.RIGHT_PAREN, "Expect ) after if condition)")
+	p.acceptToken(lexer.LEFT_PAREN, "Expect ( after 'if'")
+	condition := p.expression()
+	p.acceptToken(lexer.RIGHT_PAREN, "Expect ) after if condition")
 
-  thenBranch := p.statement()
-  var elseBranch Stmt
-  if p.match(lexer.ELSE) {
-    elseBranch = p.statement()
-  }
+	thenBranch := p.statement()
+	var elseBranch Stmt
+	if p.match(lexer.ELSE) {
+		elseBranch = p.statement()
+	}
 
-  return If{condition, thenBranch, elseBranch}
-} 
+	return If{condition, thenBranch, elseBranch}
+}
 
 func (p *Parser) whileStatement() Stmt {
-  p.acceptToken(lexer.LEFT_PAREN, "Expect ( after 'while'")
-  condition := p.expression()
-  p.acceptToken(lexer.RIGHT_PAREN, "Expect ) after condition")
-  
-  body := p.statement()
+	p.acceptToken(lexer.LEFT_PAREN, "Expect ( after 'while'")
+	condition := p.expression()
+	p.acceptToken(lexer.RIGHT_PAREN, "Expect ) after condition")
 
-  return While{condition, body}
+	body := p.statement()
+
+	return While{condition, body}
 }
 
 func (p *Parser) forStatement() Stmt {
-  p.acceptToken(lexer.LEFT_PAREN, "Expect ( after 'for'")
+	p.acceptToken(lexer.LEFT_PAREN, "Expect ( after 'for'")
 
-  var initializer Stmt
-  if p.match(lexer.SEMICOLON) {
-    initializer = nil
-  } else if p.match(lexer.VAR) {
-    initializer = p.varDeclaration()
-  } else if p.match(lexer.EQUAL) {
-    initializer = p.expressionStatement()
-  }
-  
-  var condition Expr
-  if !p.check(lexer.SEMICOLON) {
-    condition = p.expression()
-  }
-  p.acceptToken(lexer.SEMICOLON, "Expect ; after loop condition")
+	var initializer Stmt
+	if p.match(lexer.SEMICOLON) {
+		initializer = nil
+	} else if p.match(lexer.VAR) {
+		initializer = p.varDeclaration()
+	} else if p.match(lexer.EQUAL) {
+		initializer = p.expressionStatement()
+	}
 
-  var increment Expr
-  if !p.check(lexer.RIGHT_PAREN) {
-    increment = p.expression()
-  }
-  p.acceptToken(lexer.RIGHT_PAREN, "Expect ) after for clauses")
-  
-  body := p.statement()
-  if increment != nil {
-    body = Block{[]Stmt{body, Expression{increment}}}
-  }
-  if condition == nil {
-    condition = Literal{true}
-  }
-  body = While{condition, body}
+	var condition Expr
+	if !p.check(lexer.SEMICOLON) {
+		condition = p.expression()
+	}
+	p.acceptToken(lexer.SEMICOLON, "Expect ; after loop condition")
 
-  if initializer != nil {
-    body = Block{[]Stmt{initializer, body}}
-  }
+	var increment Expr
+	if !p.check(lexer.RIGHT_PAREN) {
+		increment = p.expression()
+	}
+	p.acceptToken(lexer.RIGHT_PAREN, "Expect ) after for clauses")
 
-  return body
+	body := p.statement()
+	if increment != nil {
+		body = Block{[]Stmt{body, Expression{increment}}}
+	}
+	if condition == nil {
+		condition = Literal{true}
+	}
+	body = While{condition, body}
+
+	if initializer != nil {
+		body = Block{[]Stmt{initializer, body}}
+	}
+
+	return body
+}
+
+func (p *Parser) function(kind string) Stmt {
+	name := p.acceptToken(lexer.IDENTIFIER, "Expect "+kind+" name")
+
+	p.acceptToken(lexer.LEFT_PAREN, "Expect ( after "+kind+" name")
+	var parameters []lexer.Token
+	if !p.check(lexer.RIGHT_PAREN) {
+		parameters = append(parameters, *p.acceptToken(lexer.IDENTIFIER, "Expect parameter name"))
+	}
+
+	for p.match(lexer.COMMA) {
+		parameters = append(parameters, *p.acceptToken(lexer.IDENTIFIER, "Expect parameter name"))
+		if len(parameters) > 255 {
+			p.error(p.peek(), errors.New("Can't have more than 255 parameters"))
+		}
+
+		if p.check(lexer.RIGHT_PAREN) {
+			break
+		}
+	}
+	p.acceptToken(lexer.RIGHT_PAREN, "Expect ')' after arguments")
+
+	p.acceptToken(lexer.LEFT_BRACE, "Expect '{' before "+kind+" body")
+	body := p.block()
+
+	return Function{*name, parameters, Block{body.Statements}}
+}
+
+func (p *Parser) returnStatement() Stmt {
+	keyword := p.previous()
+
+	var value Expr
+	if !p.check(lexer.SEMICOLON) {
+		value = p.expression()
+	}
+	p.acceptToken(lexer.SEMICOLON, "Expect ';' after return value")
+
+	return Return{*keyword, value}
 }
 
 func (p *Parser) expression() Expr {
-  return p.assignment()
+	return p.assignment()
 }
 
 func (p *Parser) assignment() Expr {
-  expr := p.or()
+	expr := p.or()
 
-  if p.match(lexer.EQUAL) {
-    equals := p.previous()
-    value := p.equality()
+	if p.match(lexer.EQUAL) {
+		equals := p.previous()
+		value := p.equality()
 
-    switch expr.(type) {
-    case Variable:
-      name := expr.(Variable).Name
-      return Assign{name, value}
-    }
+		switch expr.(type) {
+		case Variable:
+			name := expr.(Variable).Name
+			return Assign{name, value}
+		}
 
-    p.error(equals, errors.New("Invalid assignment target"))
-  }
+		p.error(equals, errors.New("Invalid assignment target"))
+	}
 
-  return expr
+	return expr
 }
 
 func (p *Parser) or() Expr {
-  expr := p.and()
+	expr := p.and()
 
-  for p.match(lexer.OR) {
-    operator := p.previous()
-    right := p.and()
-    expr = Logical{expr, *operator, right}
-  }
-  
-  return expr
+	for p.match(lexer.OR) {
+		operator := p.previous()
+		right := p.and()
+		expr = Logical{expr, *operator, right}
+	}
+
+	return expr
 }
 
 func (p *Parser) and() Expr {
-  expr := p.equality()
+	expr := p.equality()
 
-  for p.match(lexer.AND) {
-    operator := p.previous()
-    right := p.equality()
-    expr = Logical{expr, *operator, right}
-  }
+	for p.match(lexer.AND) {
+		operator := p.previous()
+		right := p.equality()
+		expr = Logical{expr, *operator, right}
+	}
 
-  return expr
+	return expr
 }
 
 func (p *Parser) equality() Expr {
-  expr := p.comprasion()
+	expr := p.comprasion()
 
-  for p.match(lexer.BANG_EQUAL, lexer.EQUAL_EQUAL) {
-    operator := p.previous()
-    right := p.comprasion()
+	for p.match(lexer.BANG_EQUAL, lexer.EQUAL_EQUAL) {
+		operator := p.previous()
+		right := p.comprasion()
 
-    expr = Binary{expr, *operator, right}
-  }
+		expr = Binary{expr, *operator, right}
+	}
 
-  return expr
+	return expr
 }
 
 func (p *Parser) comprasion() Expr {
-  expr := p.term()
+	expr := p.term()
 
-  for p.match(lexer.GREATER, lexer.GREATER_EQUAL, lexer.LESS, lexer.LESS_EQUAL) {
-    operator := p.previous()
-    right := p.term()
+	for p.match(lexer.GREATER, lexer.GREATER_EQUAL, lexer.LESS, lexer.LESS_EQUAL) {
+		operator := p.previous()
+		right := p.term()
 
-    expr = Binary{expr, *operator, right}
-  }
+		expr = Binary{expr, *operator, right}
+	}
 
-  return expr
+	return expr
 }
 
 func (p *Parser) term() Expr {
-  expr := p.factor()
+	expr := p.factor()
 
-  for p.match(lexer.MINUS, lexer.PLUS) {
-    operator := p.previous()
-    right := p.factor()
+	for p.match(lexer.MINUS, lexer.PLUS) {
+		operator := p.previous()
+		right := p.factor()
 
-    expr = Binary{expr, *operator, right}
-  }
+		expr = Binary{expr, *operator, right}
+	}
 
-  return expr
+	return expr
 }
 
 func (p *Parser) factor() Expr {
-  expr := p.unary()
+	expr := p.unary()
 
-  for p.match(lexer.STAR, lexer.SLASH) {
-    operator := p.previous()
-    right := p.unary()
+	for p.match(lexer.STAR, lexer.SLASH) {
+		operator := p.previous()
+		right := p.unary()
 
-    expr = Binary{expr, *operator, right}
-  }
+		expr = Binary{expr, *operator, right}
+	}
 
-  return expr
+	return expr
 }
 
 func (p *Parser) unary() Expr {
-  if p.match(lexer.BANG, lexer.MINUS) {
-    operator := p.previous()
-    right := p.unary()
+	if p.match(lexer.BANG, lexer.MINUS) {
+		operator := p.previous()
+		right := p.unary()
 
-    return Unary{*operator, right}
-  }
+		return Unary{*operator, right}
+	}
 
-  return p.primary()
+	return p.call()
+}
+
+func (p *Parser) call() Expr {
+	expr := p.primary()
+
+	for {
+		if p.match(lexer.LEFT_PAREN) {
+			expr = p.finishCall(expr)
+		} else {
+			break
+		}
+	}
+
+	return expr
+}
+
+func (p *Parser) finishCall(callee Expr) Expr {
+	var arguments []Expr
+
+	if !p.check(lexer.RIGHT_PAREN) {
+		arguments = append(arguments, p.expression())
+	}
+
+	for p.match(lexer.COMMA) {
+		arguments = append(arguments, p.expression())
+		if len(arguments) > 255 {
+			p.error(p.peek(), errors.New("Can't have more than 255 arguments"))
+		}
+
+		if p.check(lexer.RIGHT_PAREN) {
+			break
+		}
+
+	}
+	paren := p.acceptToken(lexer.RIGHT_PAREN, "Expect ')' after arguments")
+
+	return Call{callee, *paren, arguments}
 }
 
 func (p *Parser) primary() Expr {
-  if p.match(lexer.TRUE)  { return Literal{true}  }
-  if p.match(lexer.FALSE) { return Literal{false} }
-  if p.match(lexer.NIL)   { return Literal{nil}   }
-  
-  if p.match(lexer.NUMBER, lexer.STRING) {
-    return Literal{p.previous().Literal}
-  }
-  if p.match(lexer.IDENTIFIER) {
-    return Variable{*p.previous()}
-  }
+	if p.match(lexer.TRUE) {
+		return Literal{true}
+	}
+	if p.match(lexer.FALSE) {
+		return Literal{false}
+	}
+	if p.match(lexer.NIL) {
+		return Literal{nil}
+	}
 
-  if p.match(lexer.LEFT_PAREN) {
-    expr := p.expression()
-    p.acceptToken(lexer.RIGHT_PAREN, "Expect ')' after expression")
-    return Grouping{expr}
-  }
-  
-  p.parseError("Expect expression")
-  return nil
+	if p.match(lexer.NUMBER, lexer.STRING) {
+		return Literal{p.previous().Literal}
+	}
+	if p.match(lexer.IDENTIFIER) {
+		return Variable{*p.previous()}
+	}
+
+	if p.match(lexer.LEFT_PAREN) {
+		expr := p.expression()
+		p.acceptToken(lexer.RIGHT_PAREN, "Expect ')' after expression")
+		return Grouping{expr}
+	}
+
+	p.parseError("Expect expression")
+	return nil
 }
 
 func (p *Parser) errorRecovery() {
-  if err := recover(); err != nil {
-    p.error(p.peek(), fmt.Errorf("%v", err))
-    p.synchronize()
-  }
+	if err := recover(); err != nil {
+		p.error(p.peek(), fmt.Errorf("%v", err))
+		p.synchronize()
+	}
 }
 
 func (p *Parser) synchronize() {
-  p.advance()
+	p.advance()
 
-  for !p.eof() {
-    if p.previous().TokenType == lexer.SEMICOLON {
-      return
-    }
-    switch p.peek().TokenType {
-    case lexer.IF, lexer.VAR, lexer.FUN, lexer.FOR,
-         lexer.WHILE, lexer.PRINT, lexer.CLASS, lexer.RETURN:
-    }
-    p.advance()
-  }
+	for !p.eof() {
+		if p.previous().TokenType == lexer.SEMICOLON {
+			return
+		}
+		switch p.peek().TokenType {
+		case lexer.IF, lexer.VAR, lexer.FUN, lexer.FOR,
+			lexer.WHILE, lexer.PRINT, lexer.CLASS, lexer.RETURN:
+		}
+		p.advance()
+	}
 }
