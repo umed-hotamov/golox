c8328db2a05215f2d6bc1c7d0bee27d6976835b7
umed-hotamov <umed.hotamov5858@gmail.com>
2025-06-02 19:34:15 +0500

    init

--- internal/parser/helpers.go
+++ internal/parser/helpers.go
@@ -6,60 +6,59 @@ import (
 	"github.com/umed-hotamov/golox/internal/lexer"
 )
 
-
 func (p *Parser) peek() *lexer.Token {
-  return p.tokens[p.current]
+	return p.tokens[p.current]
 }
 
 func (p *Parser) eof() bool {
-  return p.peek().TokenType == lexer.EOF
+	return p.peek().TokenType == lexer.EOF
 }
 
 func (p *Parser) previous() *lexer.Token {
-  return p.tokens[p.current - 1]
+	return p.tokens[p.current-1]
 }
 
 func (p *Parser) advance() *lexer.Token {
-  if !p.eof() {
-    p.current += 1
-  }
+	if !p.eof() {
+		p.current += 1
+	}
 
-  return p.previous()
+	return p.previous()
 }
 
 func (p *Parser) check(tokenType lexer.TokenType) bool {
-  if p.eof() {
-    return false
-  }
+	if p.eof() {
+		return false
+	}
 
-  return tokenType == p.peek().TokenType
+	return tokenType == p.peek().TokenType
 }
 
-func (p *Parser) match(types... lexer.TokenType) bool {
-  for _, tokenType := range types {
-    if p.check(tokenType) {
-      p.advance()
-      return true
-    }
-  }
+func (p *Parser) match(types ...lexer.TokenType) bool {
+	for _, tokenType := range types {
+		if p.check(tokenType) {
+			p.advance()
+			return true
+		}
+	}
 
-  return false
+	return false
 }
 
 func (p *Parser) acceptToken(tokenType lexer.TokenType, message string) *lexer.Token {
-  if p.check(tokenType) {
-    return p.advance()
-  }
+	if p.check(tokenType) {
+		return p.advance()
+	}
 
-  p.parseError(message)
-  return nil
+	p.parseError(message)
+	return nil
 }
 
 func (p *Parser) parseError(message string) {
-  p.HasError = true
-  panic(message)
+	p.HasError = true
+	panic(message)
 }
 
 func (p *Parser) error(token *lexer.Token, err error) {
-  fmt.Printf("[line: %d] Error: %s\n", token.Line, err.Error())
+	fmt.Printf("[line: %d] Error: %s\n", token.Line, err.Error())
 }
