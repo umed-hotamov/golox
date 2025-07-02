3317c84f2a8396d512dcf713242a59b46817e52f
umed-hotamov <umed.hotamov5858@gmail.com>
2025-07-02 17:29:38 +0500

    add statement resolver

---
+++ internal/resolver/statement.go
@@ -0,0 +1,25 @@
+package resolver
+
+import "github.com/umed-hotamov/golox/internal/ast"
+
+func (r *Resolver) resolveStatement(statement ast.Stmt) {
+  switch statement.(type) {
+  case ast.Block:
+    r.resolveBlock(statement.(ast.Block))
+  }
+}
+
+func (r *Resolver) resolveBlock(statement ast.Block) {
+  r.beginScope()
+  r.resolve(statement.Statements)
+  r.endScope()
+}
+
+func (r *Resolver) resolveVar(statement ast.Var) {
+  r.declare(statement.Name)
+  if statement.Initializer != nil {
+    r.resolveExpression(statement.Initializer)
+  }
+
+  r.define(statement.Name)
+}
