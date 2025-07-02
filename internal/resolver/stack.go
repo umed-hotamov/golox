461830793a8e25abb3d18d76060fbf0030651f1e
umed-hotamov <umed.hotamov5858@gmail.com>
2025-07-02 17:32:39 +0500

    upd: add some methods to stack

--- internal/resolver/stack.go
+++ internal/resolver/stack.go
@@ -1,50 +1,50 @@
 package resolver
 
 type Stack struct {
-  items []any
+	items []any
 }
 
 func NewStack() *Stack {
-  return &Stack{
-    items: make([]any, 0),
-  }
+	return &Stack{
+		items: make([]any, 0),
+	}
 }
 
 func (s *Stack) Push(item any) {
-  s.items = append(s.items, item)
+	s.items = append(s.items, item)
 }
 
 func (s *Stack) Pop() any {
-  if s.IsEmpty() {
-    return nil
-  }
-  
-  last := s.items[:len(s.items) - 1]
-  s.items = s.items[:len(s.items) - 1]
+	if s.IsEmpty() {
+		return nil
+	}
 
-  return last
+	last := s.items[:len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+
+	return last
 }
 
 func (s *Stack) IsEmpty() bool {
-  return len(s.items) == 0
+	return len(s.items) == 0
 }
 
 func (s *Stack) Peek() any {
-  if s.IsEmpty() {
-    return nil
-  }
+	if s.IsEmpty() {
+		return nil
+	}
 
-  return s.items[len(s.items) - 1]
+	return s.items[len(s.items)-1]
 }
 
 func (s *Stack) Size() int {
-  return len(s.items)
+	return len(s.items)
 }
 
 func (s *Stack) Get(index int) any {
-  if s.IsEmpty() {
-    return nil
-  }
+	if s.IsEmpty() {
+		return nil
+	}
 
-  return s.items[index]
+	return s.items[index]
 }
