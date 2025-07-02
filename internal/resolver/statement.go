637b4d17400078f306c5f0e403261e314a53c771
umed-hotamov <umed.hotamov5858@gmail.com>
2025-07-02 17:32:25 +0500

    add statement resolver

--- internal/resolver/statement.go
+++ internal/resolver/statement.go
@@ -3,23 +3,23 @@ package resolver
 import "github.com/umed-hotamov/golox/internal/ast"
 
 func (r *Resolver) resolveStatement(statement ast.Stmt) {
-  switch statement.(type) {
-  case ast.Block:
-    r.resolveBlock(statement.(ast.Block))
-  }
+	switch statement.(type) {
+	case ast.Block:
+		r.resolveBlock(statement.(ast.Block))
+	}
 }
 
 func (r *Resolver) resolveBlock(statement ast.Block) {
-  r.beginScope()
-  r.resolve(statement.Statements)
-  r.endScope()
+	r.beginScope()
+	r.resolve(statement.Statements)
+	r.endScope()
 }
 
 func (r *Resolver) resolveVar(statement ast.Var) {
-  r.declare(statement.Name)
-  if statement.Initializer != nil {
-    r.resolveExpression(statement.Initializer)
-  }
+	r.declare(statement.Name)
+	if statement.Initializer != nil {
+		r.resolveExpression(statement.Initializer)
+	}
 
-  r.define(statement.Name)
+	r.define(statement.Name)
 }
