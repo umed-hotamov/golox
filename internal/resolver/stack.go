1828e21625323087147140a9442c531aa4fefb05
umed-hotamov <umed.hotamov5858@gmail.com>
2025-07-02 16:18:14 +0500

    init: add stack

---
+++ internal/resolver/stack.go
@@ -0,0 +1,38 @@
+package resolver
+
+type Stack struct {
+  items []any
+}
+
+func NewStack() *Stack {
+  return &Stack{
+    items: make([]any, 0),
+  }
+}
+
+func (s *Stack) Push(item any) {
+  s.items = append(s.items, item)
+}
+
+func (s *Stack) Pop() any {
+  if s.IsEmpty() {
+    return nil
+  }
+  
+  last := s.items[:len(s.items) - 1]
+  s.items = s.items[:len(s.items) - 1]
+
+  return last
+}
+
+func (s *Stack) IsEmpty() bool {
+  return len(s.items) == 0
+}
+
+func (s *Stack) Peek() any {
+  if s.IsEmpty() {
+    return nil
+  }
+
+  return s.items[len(s.items) - 1]
+}
