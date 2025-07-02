c14125220a1d0eaded7e557ba1df2060144af22d
umed-hotamov <umed.hotamov5858@gmail.com>
2025-07-02 17:29:28 +0500

    add expression resolver

---
+++ internal/resolver/expression.go
@@ -0,0 +1,22 @@
+package resolver
+
+import "github.com/umed-hotamov/golox/internal/ast"
+
+func (r *Resolver) resolveExpression(expression ast.Expr) {
+  switch expression.(type) {
+  case ast.Variable: 
+  }
+}
+
+func (r *Resolver) resolveVariable(expression ast.Variable) {
+  if !r.scopes.IsEmpty() {
+    scope := r.scopes.Peek().(map[string]bool)
+    if v, ok := scope[expression.Name.Lexeme]; ok {
+      if !v {
+        r.error(expression.Name, "Can't read local variable in its own initializer")
+      }
+    }
+  }
+
+  r.resolveLocal(expression, expression.Name)
+}
