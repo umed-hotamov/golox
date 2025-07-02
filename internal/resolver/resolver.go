f23525d3cb6725259dab86b5b27db76c8b6bc362
umed-hotamov <umed.hotamov5858@gmail.com>
2025-07-02 17:32:29 +0500

    upd: add helper methods

--- internal/resolver/resolver.go
+++ internal/resolver/resolver.go
@@ -9,61 +9,61 @@ import (
 )
 
 type Resolver struct {
-  interpreter *interpreter.Interpreter
-  scopes      *Stack
-  hasError    bool
+	interpreter *interpreter.Interpreter
+	scopes      *Stack
+	hasError    bool
 }
 
 func NewResolver(interpreter *interpreter.Interpreter) *Resolver {
-  return &Resolver{
-    interpreter: interpreter,
-    scopes:      NewStack(),
-  }
+	return &Resolver{
+		interpreter: interpreter,
+		scopes:      NewStack(),
+	}
 }
 
 func (r *Resolver) resolve(statements []ast.Stmt) {
-  for _, statement := range statements {
-    r.resolveStatement(statement)
-  }
+	for _, statement := range statements {
+		r.resolveStatement(statement)
+	}
 }
 
 func (r *Resolver) beginScope() {
-  r.scopes.Push(make(map[string]bool))
+	r.scopes.Push(make(map[string]bool))
 }
 
 func (r *Resolver) endScope() {
-  r.scopes.Pop()
+	r.scopes.Pop()
 }
 
 func (r *Resolver) declare(name lexer.Token) {
-  if r.scopes.IsEmpty() {
-    return
-  }
+	if r.scopes.IsEmpty() {
+		return
+	}
 
-  scope := r.scopes.Peek().(map[string]bool)
-  scope[name.Lexeme] = false
+	scope := r.scopes.Peek().(map[string]bool)
+	scope[name.Lexeme] = false
 }
 
 func (r *Resolver) define(name lexer.Token) {
-  if r.scopes.IsEmpty() {
-    return
-  }
+	if r.scopes.IsEmpty() {
+		return
+	}
 
-  scope := r.scopes.Peek().(map[string]bool)
-  scope[name.Lexeme] = true
+	scope := r.scopes.Peek().(map[string]bool)
+	scope[name.Lexeme] = true
 }
 
 func (r *Resolver) resolveLocal(expression ast.Expr, name lexer.Token) {
-  for i := r.scopes.Size() - 1; i >= 0; i-- {
-    scope := r.scopes.Get(i).(map[string]bool)
-    if _, ok := scope[name.Lexeme]; ok {
-      r.interpreter.Resolve(expression, r.scopes.Size() - 1 - i)
-      return
-    }
-  }
+	for i := r.scopes.Size() - 1; i >= 0; i-- {
+		scope := r.scopes.Get(i).(map[string]bool)
+		if _, ok := scope[name.Lexeme]; ok {
+			r.interpreter.Resolve(expression, r.scopes.Size()-1-i)
+			return
+		}
+	}
 }
 
 func (r *Resolver) error(token lexer.Token, message string) {
 	fmt.Printf("[line: %d] Error: %s\n", token.Line, message)
-  r.hasError = true
+	r.hasError = true
 }
