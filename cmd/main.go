3e2c6757ea685209f54bcab4ae9b5b3e99895fce
umed-hotamov <umed.hotamov5858@gmail.com>
2025-07-03 16:50:21 +0500

    formatted

--- cmd/main.go
+++ cmd/main.go
@@ -13,68 +13,68 @@ import (
 )
 
 func main() {
-  args := os.Args[1:]
+	args := os.Args[1:]
 
-  if len(args) > 1 {
-    fmt.Println("Usage: glox [source]")
-  }
-  
-  if len(args) == 1 {
-    runFile(args[0])
-  } else {
-    runPrompt()
-  }
+	if len(args) > 1 {
+		fmt.Println("Usage: glox [source]")
+	}
+
+	if len(args) == 1 {
+		runFile(args[0])
+	} else {
+		runPrompt()
+	}
 }
 
 func runFile(filename string) {
-  data, err := os.ReadFile(filename)
-  if err != nil {
-    log.Fatalf("failed to read file: %s", err)
-  }
-  source := string(data)
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("failed to read file: %s", err)
+	}
+	source := string(data)
 
-  interpreter := interpreter.NewInterpreter()
-  run(source, interpreter)
+	interpreter := interpreter.NewInterpreter()
+	run(source, interpreter)
 }
 
 func runPrompt() {
-  scanner := bufio.NewScanner(os.Stdin)
-  interpreter := interpreter.NewInterpreter()
-  
-  for {
-    fmt.Print("golox~~>  ")
-    
-    isNotEnd := scanner.Scan()
-    if !isNotEnd {
-      return
-    }
-    line := scanner.Text()
-    if line == "exit" {
-      break
-    }
+	scanner := bufio.NewScanner(os.Stdin)
+	interpreter := interpreter.NewInterpreter()
 
-    run(line, interpreter)
-  }
+	for {
+		fmt.Print("golox~~>  ")
+
+		isNotEnd := scanner.Scan()
+		if !isNotEnd {
+			return
+		}
+		line := scanner.Text()
+		if line == "exit" {
+			break
+		}
+
+		run(line, interpreter)
+	}
 }
 
 func run(source string, interpreter *interpreter.Interpreter) {
-  lexer := lexer.NewLexer(source)
-  tokens := lexer.Lex()
+	lexer := lexer.NewLexer(source)
+	tokens := lexer.Lex()
 
-  parser := parser.NewParser(tokens)
-  statements := parser.Parse()
-  if lexer.HasError {
-    return
-  }
-  if parser.HasError {
-    return
-  }
+	parser := parser.NewParser(tokens)
+	statements := parser.Parse()
+	if lexer.HasError {
+		return
+	}
+	if parser.HasError {
+		return
+	}
 
-  resolver := resolver.NewResolver(interpreter)
-  resolver.Resolve(statements)
-  if resolver.HasError {
-    return
-  }
+	resolver := resolver.NewResolver(interpreter)
+	resolver.Resolve(statements)
+	if resolver.HasError {
+		return
+	}
 
-  interpreter.Interpret(statements)
+	interpreter.Interpret(statements)
 }
